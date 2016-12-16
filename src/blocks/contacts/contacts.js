508acc03c9d6fbccf995df2f6202eca715ed9b40
Artem Verbat <artem.verbat@gmail.com>
2016-12-16 14:27:42 +0200

    Fixed contact form.

--- src/blocks/contacts/contacts.js
+++ src/blocks/contacts/contacts.js
@@ -4,29 +4,34 @@ import $ from 'jquery';
 $('.contacts__form').submit(function (e) {
     e.preventDefault();
 
-    const formElements = $(this.elements);
-    const name = (this.elements['name']).val();
-    const email = (this.elements['email']).val();
-    const message = (this.elements['message']).val();
-    const page = ''+(this.elements['page']).val();
-    const _gotcha = (this.elements['_gotcha']).val();
+    const name = $(this.elements['name']).val();
+    const email = $(this.elements['email']).val();
+    const message = $(this.elements['message']).val();
+    const page = location.href;
+    const _gotcha = $(this.elements['_gotcha']).val();
 
     if (_gotcha) return;
 
     $.ajax({
         url: 'https://formspree.io/wtf.noword@gmail.com',
         method: 'POST',
+        dataType: 'json',
         data: {
             _subject: 'From strata',
             _format: 'plain',
             _gotcha,
+
             page,
             email,
             name,
             message
         }
     })
-    .done(function() {
-        formElements.val('');
+    .done(() => {
+        // Message sended
+    }).fail(() => {
+        // Message dont sended
+    }).always(() => {
+        this.reset();
     });
 });
