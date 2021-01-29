ba5c89d72622b00ddd154fa633c26f7b37cf7b0f
Artem Verbat <artem.verbat@gmail.com>
2021-01-29 23:55:27 +0200

    style: Remove redundant space.

--- src/helpers/getFormFields.js
+++ src/helpers/getFormFields.js
@@ -1,7 +1,7 @@
 export const getFormFields = (formElement) => {
   const formFields = Array.from(formElement.querySelectorAll('[name]'));
 
-  return  formFields.reduce((values, { name, value }) => {
+  return formFields.reduce((values, { name, value }) => {
     values[name] = value;
 
     return values;
